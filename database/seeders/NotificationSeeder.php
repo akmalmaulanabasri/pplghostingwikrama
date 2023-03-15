060326ba2a0bf93fb8a35549fed35fcd97803889
Akmal Maulana Basri <64055432+akmalmaulanabasri@users.noreply.github.com>
2023-03-15 17:15:17 +0700

    notif

--- database/seeders/NotificationSeeder.php
+++ database/seeders/NotificationSeeder.php
@@ -15,15 +15,13 @@ class NotificationSeeder extends Seeder
      */
     public function run()
     {
-        $userId = User::whereEmail('superadmin@laraveltemplate.com')->first()->id ?? 1;
-        foreach (range(1, 20) as $i) {
-            Notification::create([
-                'title'             => 'Test title',
-                'content'           => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima doloremque, ullam pariatur vero beatae tempora dolor qui autem, similique consequatur iure explicabo. Magnam temporibus blanditiis, nesciunt iusto eius explicabo quae?',
-                'user_id'           => $userId,
-                'is_read'           => false,
-                'notification_type' => 'transaksi masuk',
-            ]);
-        }
+        $userId = User::whereEmail('akmal@gmail.com')->first()->id ?? 1;
+        Notification::create([
+            'title' => 'Selamat Datang',
+            'content' => 'Terimakasih sudah menggunakan PPLG HOSTING, silahkan gunakan dengan baik, Terimakasih. ~akmal',
+            'user_id' => $userId,
+            'is_read' => false,
+            'notification_type' => 'Welcome',
+        ]);
     }
 }
