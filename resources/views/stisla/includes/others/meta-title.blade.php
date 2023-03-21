be201cd1c251f816c5826d7af679281bea21155a
Akmal Maulana Basri <64055432+akmalmaulanabasri@users.noreply.github.com>
2023-03-21 09:22:21 +0700

    meta google

--- resources/views/stisla/includes/others/meta-title.blade.php
+++ resources/views/stisla/includes/others/meta-title.blade.php
@@ -3,6 +3,7 @@
 <meta name="keywords" content="{{ $_meta_keywords }}">
 <meta name="author" content="{{ $_meta_author }}">
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+<meta name="google-site-verification" content="FV6uH3uqHEWbV7QPStPWn5LD3NmDFCfp_yNFIx0nciI" />
 <title>@yield('title') &mdash; {{ $_app_name }}</title>
 <!-- Favicon -->
 <link rel="shortcut icon" href="{{ $_favicon ?? asset('favicon.ico') }}" type="image/x-icon">
